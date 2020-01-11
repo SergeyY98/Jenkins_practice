8d95a2eeabb8ce819943d1f6bd79cab7e6bee4ca
quicksilver32 <ameliya9@mail.ru>
2020-01-11 14:45:20 +0300

    Add mistake

--- src/main/java/testproject/Aggregator.java
+++ src/main/java/testproject/Aggregator.java
@@ -2,7 +2,7 @@ package testproject;
 
 /** Class that aggregates a lot of values into one summ */
 class Aggregator {
-    private char sum = 0;
+    private int sum = 0;
 
     /**
      * Add a value to the aggregated number.
